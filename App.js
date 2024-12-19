7a196046e37ab4f200702bae0d2805ff177cac16
Muchiri-cmd <davismuchiri21@gmail.com>
2024-12-19 15:54:13 +0300

    (Ex 10.11) - Fetch Repos Using apolloClient   - Replace Fetch API implementation in useRepos hook with GraphQL query   - Structure Graphql folder (queries,mutations,queries)

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import Main from "./src/components/Main";
 import { NativeRouter } from 'react-router-native';
 import { StatusBar } from 'expo-status-bar';
 
+import { ApolloProvider } from "@apollo/client";
+import createApolloClient from "./src/utils/apolloClient";
+
+const apolloClient = createApolloClient();
+
 const App = () => {
   return ( 
     <>
@@ -11,7 +16,9 @@ const App = () => {
           v7_relativeSplatPath: true,
         }}
       >
-        <Main />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
       <StatusBar style="auto" />
     </>
