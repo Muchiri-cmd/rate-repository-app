54cd27b3a6692c61e1a3d4941328f6b6eb173f34
Muchiri-cmd <davismuchiri21@gmail.com>
2024-12-19 09:27:04 +0300

    (Ex 10.8) - Create the sign-in form

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,72 @@
-import Text from './Text';
+import { Button,TextInput,View,StyleSheet} from 'react-native';
+import { useFormik } from 'formik';
+import theme from './theme';
+
+const initialValues = {
+  username : '',
+  password : '',
+}
+
+const LoginForm = ({ onSubmit }) => {
+  const formik = useFormik({
+    initialValues,
+    onSubmit
+  });
+
+  return (
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value = {formik.values.username}
+        onChangeText={formik.handleChange('username')}
+      />
+      <TextInput 
+        style={styles.input}
+        placeholder = "Password"
+        secureTextEntry = {true}
+        value = { formik.values.password}
+        onChangeText = { formik.handleChange('password')}
+      />
+      <Button 
+        onPress={formik.handleSubmit}
+        title="Sign in"
+      />
+      
+    </View>
+  )
+}
+
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = values => {
+    const username = values.username
+    const password = values.password
+
+    if (password && username ){
+      console.log(`password: ${password} \n username:${username}`)
+    } else {
+      console.log(`Kindly enter correct login credentials`)
+    }
+  }
+  
+  return <LoginForm onSubmit={onSubmit} />
 };
 
+const styles = StyleSheet.create({
+  form : {
+    backgroundColor:theme.colors.white,
+    padding:15,
+    display:'flex',
+  },
+  input:{
+    height:40,
+    margin:12,
+    borderColor: 'gray',
+    borderRadius:8,
+    borderWidth:1,
+    padding:10,
+  }
+ 
+})
 export default SignIn;
